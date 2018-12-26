3e75b6664dead01bfe826819921fd03822f75f92
Ville-Matti Hakanpää <villematti@villematti.info>
2018-12-27 00:37:42 +0200

    Complete the tutorial

--- app/main.js
+++ app/main.js
@@ -1,1 +1,179 @@
-console.log('This is an Electron application!');
+const fs = require('fs');
+
+const { 
+    app,
+    BrowserWindow,
+    dialog,
+    Menu
+} = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        show: false
+    });
+
+    Menu.setApplicationMenu(applicationMenu);
+
+    mainWindow.loadFile(`${__dirname}/index.html`);
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
+});
+
+const getFileFromUser = exports.getFileFromUser = () => {
+    const files = dialog.showOpenDialog({
+        properties: ['openFile'],
+        buttonLabel: 'Unveil',
+        title: 'Open Firesale Document',
+        filters: [
+            {
+                name: 'Markdown Files', 
+                extensions: ['md', 'mdown', 'markdown', 'marcdown']
+            },
+            {
+                name: 'Text Files', 
+                extensions: ['txt', 'text']
+            }
+        ]
+    });
+
+    if (!files) return;
+
+    const file = files[0];
+
+    openFile(file);
+};
+
+exports.saveMarkdown = (filePath, content) => {
+    if (!filePath) {
+        filePath = dialog.showSaveDialog(mainWindow, {
+            title: 'Save Markdown',
+            defaultPath: app.getPath('desktop'),
+            filters: [
+                {
+                    name: 'Markdown File',
+                    extensions: ['md', 'markdown', 'mdown', 'marcdown']
+                }
+            ]
+        });
+    }
+
+    if(!filePath) return;
+
+    fs.writeFileSync(filePath, content);
+
+    app.addRecentDocument(filePath);
+
+    mainWindow.webContents.send('file-opened', filePath, content);
+};
+
+exports.saveHtmlFile = (content) => {
+    filePath = dialog.showSaveDialog(mainWindow, {
+        title: 'Export HTML',
+        defaultPath: app.getPath('desktop'),
+        buttonLabel: 'Export',
+        filters: [
+            {
+                name: 'HTML File',
+                extensions: ['htm', 'html']
+            }
+        ]
+    });
+
+    if(!filePath) return;
+
+    fs.writeFileSync(filePath, content);
+
+    mainWindow.webContents.send('html-saved', filePath);
+};
+
+const openFile = exports.openFile = (filePath) => {
+    if(!filePath) return;
+
+    const content = fs.readFileSync(filePath).toString();
+    
+    app.addRecentDocument(filePath);
+    
+    mainWindow.webContents.send('file-opened', filePath, content);
+};
+
+const template = [
+    {
+        label: 'File',
+        submenu: [
+            {
+            label: 'Open File',
+            accelerator: 'CommandOrControl+O',
+            click() {
+                    getFileFromUser();
+                }
+            },
+            {
+                label: 'Save Markdown',
+                accelerator: 'CommandOrControl+S',
+                click() {
+                    mainWindow.webContents.send('save-file');
+                }
+            },
+            {
+                label: 'Export HTML',
+                accelerator: 'Alt+E',
+                click() {
+                    mainWindow.webContents.send('save-html');
+                }
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Undo',
+                role: 'undo'
+            },
+            {
+                label: 'Redo',
+                role: 'redo'
+            },
+            {
+                label: 'Copy',
+                role: 'copy'
+            },
+            {
+                label: 'Cut',
+                role: 'cut'
+            },
+            {
+                label: 'Paste',
+                role: 'paste'
+            },
+            {
+                label: "Select all",
+                role: 'selectAll'
+            }
+        ]
+    }
+];
+
+if (process.platform === 'darwin') {
+
+    const applicationName = 'Fire Sale';
+    template.unshift({
+        label: applicationName,
+        submenu: [
+            {
+                label: `About ${applicationName}`,
+                role: 'about'
+            },
+            {
+                label: `Quit ${applicationName}`,
+                role: 'quit'
+            }
+        ]
+    });
+}
+
+const applicationMenu = Menu.buildFromTemplate(template);
--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,13 @@
+const path = require('path');
+
+const { remote, ipcRenderer, shell } = require('electron');
+
+const mainProcess = remote.require('./main');
+const currentWindow = remote.getCurrentWindow();
+
+let filePath = null;
+let originalContent = '';
+
 const marked = require('marked');
 
 const markdownView = document.querySelector('#markdown');
@@ -14,7 +24,137 @@ const renderMarkdownToHtml = markdown => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const updateUserInterface = (isEdited) => {
+  isEdited = isEdited ? true : false;
+
+  let title = 'Fire Sale';
+
+  const edited =  isEdited ? "*" : "";
+  saveMarkdownButton.disabled =  !isEdited;
+  revertButton.disabled = !isEdited;
+
+  if (filePath) {
+    title = `${path.basename(filePath)} - ${title}`;
+
+    currentWindow.setRepresentedFilename(filePath);
+  }
+
+  showFileButton.disabled = !filePath;
+  openInDefaultButton.disabled = !filePath;
+
+  currentWindow.setDocumentEdited(isEdited);
+
+  title = `${edited}${title}`;
+
+  currentWindow.setTitle(title);
+};
+
+newFileButton.addEventListener('click', () => {
+  filePath = null;
+  originalContent = "";
+  markdownView.value = "";
+  renderMarkdownToHtml("");
+});
+
+const saveHtml = () => {
+  const content = htmlView.innerHTML;
+
+  mainProcess.saveHtmlFile(content);
+};
+
+saveHtmlButton.addEventListener('click', saveHtml);
+
 markdownView.addEventListener('keyup', event => {
   const currentContent = event.target.value;
+
   renderMarkdownToHtml(currentContent);
+  
+  updateUserInterface(currentContent !== originalContent);
 });
+
+ipcRenderer.on('save-html', saveHtml);
+
+openFileButton.addEventListener('click', () => {
+  mainProcess.getFileFromUser();
+});
+
+const saveMarkdown = () => {
+  mainProcess.saveMarkdown(filePath, markdownView.value);
+};
+
+saveMarkdownButton.addEventListener('click', saveMarkdown);
+
+ipcRenderer.on('save-file', saveMarkdown);
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  filePath = file;
+  originalContent = content;
+  
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+
+  updateUserInterface();
+});
+
+showFileButton.addEventListener('click', () => {
+  if (!filePath) {
+    return alert("No file selected!");
+  }
+
+  shell.showItemInFolder(filePath);
+});
+
+openInDefaultButton.addEventListener('click' , () => {
+  if (!filePath) {
+    return alert("No file selected!");
+  }
+
+  shell.openItem(filePath);
+});
+
+ipcRenderer.on('html-saved', (event, file) => {
+  alert(`HTML saved to ${file}`);
+});
+
+document.addEventListener('dragstart', event => event.preventDefault());
+document.addEventListener('dragover', event => event.preventDefault());
+document.addEventListener('dragleave', event => event.preventDefault());
+document.addEventListener('drop', event => event.preventDefault());
+
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+const getDroppedFile = (event) => event.dataTransfer.files[0];
+const fileTypeisSupported = (file) => {
+  return ['text/plain', 'text/markdown'].includes(file.type);
+};
+
+markdownView.addEventListener('dragover', (event) => {
+  const file = getDraggedFile(event);
+
+  if(fileTypeisSupported(file)) {
+    markdownView.classList.add('drag-over');
+  } else {
+    markdownView.classList.add('drag-error');
+  }
+});
+
+markdownView.addEventListener('dragleave', (event) => {
+  removeDragClasses();
+});
+
+markdownView.addEventListener('drop', (event) => {
+
+  const file = getDroppedFile(event);
+
+  if (fileTypeisSupported(file)) {
+    mainProcess.openFile(file.path);
+  } else {
+    alert('That file type is not supported!');
+  }
+
+  removeDragClasses();
+});
+
+const removeDragClasses = () => {
+  markdownView.classList.remove('drag-over');
+  markdownView.classList.remove('drag-error');
+};
